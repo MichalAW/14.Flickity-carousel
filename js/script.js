7ae4d1702fc8fffbf629d53512186bd41c02e0ca
MichalAW <michal.wanta@gmail.com>
2019-03-06 16:42:21 +0100

    flickity task

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ var flkty = new Flickity( elem, {
  	contain: true,
   	prevNextButtons: false,
 	pageDots: false,
-	hash: true
+	hash: true,
+});
+
+var flkty = new Flickity('.carousel');
+
+var previousButton = document.querySelector('.button-previous');
+previousButton.addEventListener( 'click', function() {
+  flkty.previous();
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
