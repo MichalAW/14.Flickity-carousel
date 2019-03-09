1d11927e6d52e2747866673a4abf785aa4e488e6
MichalAW <michal.wanta@gmail.com>
2019-03-09 19:40:19 +0100

    fix html and js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// Mustache
+var templateList = document.getElementById('template-slider').innerHTML;
+var templateItem = document.getElementById('template-slider-item').innerHTML;
+
+// Następnie zoptymalizujemy drugą z nich, ponieważ tylko ona będzie wykonywana wielokrotnie.
+Mustache.parse(templateItem);
+// Teraz stworzymy zmienną, w której chcemy mieć kod HTML wszystkich produktów.
+var listItems = '';
+
+for(var i = 0; i < data.length; i++){
+	listItems += Mustache.render(templateItem, data[i]);
+}
+
+var fullProductList = Mustache.render(templateList, {slider: listItems});
+results.insertAdjacentHTML('beforeend', fullProductList);
+
+//////////////
+
 var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
