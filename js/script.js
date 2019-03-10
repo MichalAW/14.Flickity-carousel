d8798350c84ff5f89ddf0147f5d649aab665dc50
MichalAW <michal.wanta@gmail.com>
2019-03-10 12:48:35 +0100

    finish task Map_with_markers

--- js/script.js
+++ js/script.js
@@ -43,14 +43,107 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
-//google map function
-// Initialize and add the map
-window.initMap = function() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+(function(){ 
+
+    window.initMap = function() {
+
+    var initCoords = data[0]['coords'];
+
+    var uluru = {lat: -25.363, lng: 131.044};
+    var sydney = {lat: -33.874237, lng: 151.198517};
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: initCoords
+    });
+    
+    for (var i=0; i < data.length;i++) {
+        new google.maps.Marker({
+        position: data[i]['coords'],
+        map: map
+      });
+    }
+
+    document.getElementById('center-map').addEventListener('click', function(event){
+      event.preventDefault();
+
+      map.panTo(sydney);
+      
+      map.setZoom(10);
+    });
+    
+    document.getElementById('center-smooth').addEventListener('click', function(event){
+      event.preventDefault();
+      smoothPanAndZoom(map, 7, uluru);
+    });
+  } 
+  
+  var smoothPanAndZoom = function(map, zoom, coords){
+
+    var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+    jumpZoom = Math.min(jumpZoom, zoom -1);
+    jumpZoom = Math.max(jumpZoom, 3);
+
+    smoothZoom(map, jumpZoom, function(){
+
+      smoothPan(map, coords, function(){
+ 
+        smoothZoom(map, zoom); 
+      });
+    });
+  };
+  
+  var smoothZoom = function(map, zoom, callback) {
+    var startingZoom = map.getZoom();
+    var steps = Math.abs(startingZoom - zoom);
+    
+    if(!steps) {
+      
+      if(callback) {
+
+        callback();
+      }
+
+      return;
+    }
+    
+    var stepChange = - (startingZoom - zoom) / steps;
+
+    var i = 0;
+
+    var timer = window.setInterval(function(){
+      
+      if(++i >= steps) {
+        
+        window.clearInterval(timer);
+        
+        if(callback) {
+          
+          callback();
+        }
+      }
+    
+      map.setZoom(Math.round(startingZoom + stepChange * i));
+    }, 80);
+  };
+
+  var smoothPan = function(map, coords, callback) {
+    var mapCenter = map.getCenter();
+    coords = new google.maps.LatLng(coords);
+
+    var steps = 12;
+    var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
+
+    var i = 0;
+    var timer = window.setInterval(function(){
+      if(++i >= steps) {
+        window.clearInterval(timer);
+        if(callback) callback();
+      }
+      map.panTo({lat: mapCenter.lat() + panStep.lat * i, lng: mapCenter.lng() + panStep.lng * i});
+    }, 1000/30);
+  }; 
+  
+})();  
+
+initMap();
