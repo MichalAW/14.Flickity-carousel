1d8b2c191c4c569724ad489e2fb9814a3f37185a
MichalAW <michal.wanta@gmail.com>
2019-03-12 00:54:45 +0100

    fix 3 functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
-// Mustache
-var templateList = document.getElementById('template-slider').innerHTML;
-var templateItem = document.getElementById('template-slider-item').innerHTML;
+// rendering the html with a mustache
+mustacheRender();
 
-// Optimaize the second one, becouse it it will be performed repeatedly.
-Mustache.parse(templateItem);
-// Now we will create a variable in which we want to have the HTML code of all products.
-var listItems = '';
+// variables to the fltor slider - globally, because used in the flktSlider () and initMap () functions
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity(elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+    prevNextButtons: false,
+  pageDots: false,
+  hash: true,
+});
 
-for(var i = 0; i < data.length; i++){
-	listItems += Mustache.render(templateItem, data[i]);
+function mustacheRender() {
+  var templateList = document.getElementById('template-slider').innerHTML;
+  var templateItem = document.getElementById('template-slider-item').innerHTML;
+
+  // Then we will optimize the second one, because only it will be performed repeatedly.
+  Mustache.parse(templateItem);
+  // Now we will create a variable in which we want to have the HTML code of all products.
+  var listItems = '';
+
+  for(var i = 0; i < data.length; i++){
+    listItems += Mustache.render(templateItem, data[i]);
+  }
+
+  var fullList = Mustache.render(templateList, {slider: listItems});
+  results.insertAdjacentHTML('beforeend', fullList);
 }
 
-var fullList = Mustache.render(templateList, {slider: listItems});
-results.insertAdjacentHTML('beforeend', fullList);
+function flktySlider() {
+  var nextButton = document.querySelector('.button-next');
+  nextButton.addEventListener('click', function() {
+    flkty.next();
+  });
 
-//////////////
+  var previousButton = document.querySelector('.button-previous');
+  previousButton.addEventListener('click', function() {
+    flkty.previous();
+  });
 
-var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
-  // our options
- 	cellAlign: 'left',
- 	contain: true,
-  	prevNextButtons: false,
-	pageDots: false,
-	hash: true,
-});
+  var progressBar = document.querySelector('.progress-bar')
 
-var nextButton = document.querySelector('.button-next');
-nextButton.addEventListener( 'click', function() {
-  flkty.next();
-});
+  flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+  });
+}
 
-var previousButton = document.querySelector('.button-previous');
-previousButton.addEventListener( 'click', function() {
-  flkty.previous();
-});
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
+flktySlider();
