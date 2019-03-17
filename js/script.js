fc9e8560b91a4bcabf6752965f53049775ee3463
MichalAW <michal.wanta@gmail.com>
2019-03-17 18:14:38 +0100

    all-in-one

--- js/script.js
+++ js/script.js
@@ -1,13 +1,51 @@
-// rendering the html with a mustache
+var data = [
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Las_Widok_5.JPG',
+    title: 'Title 1',
+    description: 'My description 1',
+    numb: 1,
+    coords: {lat: -25.363, lng: 131.044}
+  },
+  {
+    image: 'http://navtur.pl/files/plc_pano/4557.jpg',
+    title: 'Title 2',
+    description: 'My description 2',
+    numb: 2,
+    coords: {lat: -20.363, lng: 111.044}
+  },
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Las_mlocinski.jpg/240px-Las_mlocinski.jpg',
+    title: 'Title 3',
+    description: 'My description 3',
+    numb: 3,
+    coords: {lat: -15.363, lng: 118.044}
+  },
+  {
+    image: 'https://i.imged.pl/obraz-las-zima-piowart.jpg',
+    title: 'Title 4',
+    description: 'My description 4',
+    numb: 4,
+    coords: {lat: -13.363, lng: 141.044}
+  },
+  {
+    image: 'https://www.tapeciarnia.pl/tapety/normalne/246355_jez_liscie_drzewo_las_jesien.jpg',
+    title: 'Title 5',
+    description: 'My description 5',
+    numb: 5,
+    coords: {lat: -27.363, lng: 111.044}
+  }
+];
+// HTML mustache using the mustache to be able to manipulate the slider's html elements.
 mustacheRender();
 
-// variables to the fltor slider - globally, because used in the flktSlider () and initMap () functions
 var elem = document.querySelector('.carousel');
+var nextButton = document.querySelector('.button-next');
+var previousButton = document.querySelector('.button-previous');
+var progressBar = document.querySelector('.progress-bar');
 var flkty = new Flickity(elem, {
-  // options
   cellAlign: 'left',
   contain: true,
-    prevNextButtons: false,
+  prevNextButtons: false,
   pageDots: false,
   hash: true,
 });
@@ -16,9 +54,7 @@ function mustacheRender() {
   var templateList = document.getElementById('template-slider').innerHTML;
   var templateItem = document.getElementById('template-slider-item').innerHTML;
 
-  // Then we will optimize the second one, because only it will be performed repeatedly.
   Mustache.parse(templateItem);
-  // Now we will create a variable in which we want to have the HTML code of all products.
   var listItems = '';
 
   for(var i = 0; i < data.length; i++){
@@ -30,17 +66,13 @@ function mustacheRender() {
 }
 
 function flktySlider() {
-  var nextButton = document.querySelector('.button-next');
   nextButton.addEventListener('click', function() {
     flkty.next();
   });
 
-  var previousButton = document.querySelector('.button-previous');
   previousButton.addEventListener('click', function() {
     flkty.previous();
   });
-
-  var progressBar = document.querySelector('.progress-bar')
 
   flkty.on( 'scroll', function( progress ) {
     progress = Math.max( 0, Math.min( 1, progress ) );
@@ -49,7 +81,6 @@ function flktySlider() {
 }
 
 function initMap() {
-  // coordinates from the first slide
   var initCoords = data[0]['coords'];
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
@@ -69,82 +100,63 @@ function initMap() {
     })(i);
   }
 
-  // Then we add the action to the button, exactly the same as we did in the previous module.
   document.getElementById('center-map').addEventListener('click', function(event){
     event.preventDefault();
-    // First, we use the panTo method in the map object to move the coordinates of the map:
-    map.panTo(sydney);
-    
-    // And then we change the magnification of the map:
+    map.panTo(initCoords);
     map.setZoom(10);
   });
 
   document.getElementById('center-smooth').addEventListener('click', function(event){
     event.preventDefault();
-    smoothPanAndZoom(map, 7, uluru);
+    smoothPanAndZoom(map, 7, initCoords);
+  });
+
+  flkty.on( 'change', function(index) {
+    smoothPanAndZoom(map, 3, data[index].coords);
   });
 } 
 
-var smoothPanAndZoom = function(map, zoom, coords){
-// A little calculation to calculate the appropriate zoom to which the map at the beginning of the animation should move away.
-var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
-jumpZoom = Math.min(jumpZoom, zoom -1);
-jumpZoom = Math.max(jumpZoom, 3);
+function smoothPanAndZoom (map, zoom, coords){
+  var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+  jumpZoom = Math.min(jumpZoom, zoom -1);
+  jumpZoom = Math.max(jumpZoom, 3);
 
-// We start from the distance of the map to the calculated magnification. 
-smoothZoom(map, jumpZoom, function(){
-  // Then, move the map to the desired coordinates.
-  smoothPan(map, coords, function(){
-    // At the end, we enlarge the map to the desired magnification.
-    smoothZoom(map, zoom); 
+  smoothZoom(map, jumpZoom, function(){
+    smoothPan(map, coords, function(){
+      smoothZoom(map, zoom); 
+    });
   });
-});
 };
 
-var smoothZoom = function(map, zoom, callback) {
+function smoothZoom (map, zoom, callback) {
   var startingZoom = map.getZoom();
   var steps = Math.abs(startingZoom - zoom);
 
-  // If steps == 0, so startingZoom == zoom
   if(!steps) {
-    // If the third argument is given
     if(callback) {
-      // Call the function given as the third argument.
       callback();
     }
-    // Exit the function
     return;
   }
 
-  // A bit of mathematics, thanks to which we get -1 or 1, depending on whether startingZoom is smaller than zoom
   var stepChange = - (startingZoom - zoom) / steps;
-
   var i = 0;
-  // We call setInterval, which will execute the function every X milliseconds (X given as the second argument, in our case 80)
   var timer = window.setInterval(function(){
-    // If the right number of steps has been taken
     if(++i >= steps) {
-      // Clear the timer, i.e., stop the function given in the setInterval above
       window.clearInterval(timer);
-      // If the third argument is given
       if(callback) {
-        // Execute the function given as the third argument
         callback();
       }
     }
-    // Use the setZoom method of the map object to change the magnification to the rounded result of the calculation below
     map.setZoom(Math.round(startingZoom + stepChange * i));
   }, 80);
 };
 
-// The following function works very much like smoothZoom. Try to analyze it yourself.
-var smoothPan = function(map, coords, callback) {
+function smoothPan (map, coords, callback) {
   var mapCenter = map.getCenter();
   coords = new google.maps.LatLng(coords);
-
   var steps = 12;
   var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
-
   var i = 0;
   var timer = window.setInterval(function(){
     if(++i >= steps) {
@@ -155,5 +167,7 @@ var smoothPan = function(map, coords, callback) {
   }, 1000/30);
 };
 
-flktySlider();
-initMap();
+(function(){
+  flktySlider();
+  initMap();
+})();
